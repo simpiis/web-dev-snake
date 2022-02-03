0c7e2cafc60fb6ce174ceff4c15856d4e1287621
simpiis <simon.ejderbrand@hotmail.com>
2022-02-03 20:12:33 +0100

    user input direction function
    file for user input
    import in snake.js for user input as well as "getter" for the direction rather than the hardcoded values towards the end of update() function

--- snake.js
+++ snake.js
@@ -1,17 +1,20 @@
+import { getInputDirection } from './input.js';
+
 export const SNAKE_SPEED = 2;
 const snakeBody = [
-  { x: 10, y: 11 },
+  
   { x: 11, y: 11 },
-  { x: 12, y: 11 }
+  
 ]
 
 
 export function update() {
+ const inputDirection = getInputDirection();
   for (let i = snakeBody.length - 2; i >= 0; i--) {
     snakeBody[i + 1] = { ...snakeBody[i] }   // move every segment of the snake forward one position
   }
-  snakeBody[0].x += 1;
-  snakeBody[0].y += 0;
+  snakeBody[0].x += inputDirection.x;
+  snakeBody[0].y += inputDirection.y;
 }
 
 export function draw(gameBoard) {
