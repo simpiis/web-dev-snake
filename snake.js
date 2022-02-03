0955fa25194a0535d51128dab96ea27c1eb5c28a
simpiis <simon.ejderbrand@hotmail.com>
2022-02-03 19:49:36 +0100

    part of draw snake
    Pass gameBoard so we can append child and print later.
    
    Go through each "square" of the snake and set the class to snake so that the style in the css file applies. also set the positions in the game grid.

--- snake.js
+++ snake.js
@@ -1,10 +1,18 @@
 export const SNAKE_SPEED = 2;
+const snakeBody = [{ x: 11, y: 11 }]  // start the snake center screen
 
 
 export function update() {
-  
+  console.log("update snake")
 }
 
-export function draw() {
-  
+export function draw(gameBoard) {
+  // go through each "square" of the snake
+  snakeBody.forEach(segment => {
+    const snakeElement = document.createElement('div');
+    snakeElement.style.gridRowStart = segment.x;
+    snakeElement.style.gridColumnStart = segment.y;
+    snakeElement.classList.add('snake');
+    gameBoard.appendChild(snakeElement);
+  });
 }
