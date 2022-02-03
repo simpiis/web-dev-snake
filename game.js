9236975df62003e95b6280c20433ef994019c4b0
simpiis <simon.ejderbrand@hotmail.com>
2022-02-03 19:30:32 +0100

    game loop speed
    game loop speed currently lets the snake move 2 times per second via the SNAKE_SPEED variable.

--- game.js
+++ game.js
@@ -1,8 +1,13 @@
-
+let lastRenderTime = 0;
+const SNAKE_SPEED = 2; // move X times per second
 
 //game loop
 function main(currentTime) {
+  const secondsSinceLastRender = (currentTime - lastRenderTime) / 1000
+  if (secondsSinceLastRender < 1 / SNAKE_SPEED) return; // game loop speed check
+
   window.requestAnimationFrame(main);
+  lastRenderTime = currentTime;
 }
 
 // start loop first time
