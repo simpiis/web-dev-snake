1cda381af7c9efd0c6c1b380cdc8cbd4a9338413
simpiis <simon.ejderbrand@hotmail.com>
2022-02-22 23:03:42 +0100

    save scores to localstorage, print them to the screen
    sortScores sorts in a descending order. ONLY SAVES 5 SCORES.
    
    printScores gets the items from local storage and iterates over them to print in a <article> <p> format

--- hiscore.js
+++ hiscore.js
@@ -6,42 +6,90 @@ let totalScore = 0;
   totalScore = score;
   document.getElementById('score-field').innerText = "Your score: " + score;
   document.getElementById('submit-score-button').addEventListener("click", submitScore);
-
+  document.getElementById('restart-game-button').addEventListener("click", restartGame)
 }
 
+
+function restartGame() {
+  document.location.reload(true);
+}
 
 async function submitScore() {
   let username = document.getElementById('username-text-field').value;
   
   document.getElementById('score-form').innerHTML = '';
-  console.log("USERNAME: " + username);
   
-   await readHiScores();
+
+
+ 
+
+  sortScores(username)
+  printScores();
+
   
 
 }
+  
+
+ 
+function printScores() {
 
 
-async function readHiScores() {
-  let rawData = await fetch('./hiScore.json');
-  let hiScores = await rawData.json();
+  let scoreArr = JSON.parse(localStorage.getItem('scores'));
+  
+
+  
+
+
 
   let html = '';
 
-  console.log(hiScores);
-
-  for (let hiScore of hiScores) {
-    console.log(hiScore);
+  for (let hiScore of scoreArr) {
+    
     html += `
     <article>
-    <p>${hiScore.username}       ${hiScore.score}</p>
+    <p>${hiScore.username} - ${hiScore.score}</p>
     </article>`;
   }
-  
- document.getElementById('display-json').innerHTML = html;
+
+  document.getElementById('display-json').innerHTML = html;
   document.getElementById('display-json').style.display = 'inline';
- 
 
   
+}
+
+
+function sortScores(username) {
+  
+  const scores = (() => {
+    const value = localStorage.getItem('scores');
+    return value === null ? [] : JSON.parse(value);
+  })();
+
+  scores.push({ username: username, score: totalScore });
+
+  scores.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
+  while (scores.length > 5) {
+    scores.pop();
+  }
+
+
+  
+
+  
+
+  
+
+  localStorage.setItem('scores', JSON.stringify(scores));
+
+
+  console.log(localStorage.getItem('scores'));
+
+
+
+
 
 }
